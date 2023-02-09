5d730afc5aafcec2531a1c1ff97b2dc2d47212ed
kien tran <kientv@dinoland.io>
2023-02-09 23:16:07 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const exprss = require('express');
+const path = require('path')
+const morgan = require('morgan');
+const handlebars = require('express-handlebars')
+const hbs = require('express-handlebars')
+const app = exprss()
+const port = 3000
+
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+app.use(morgan('combined'))
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+
+app.listen(port, () => console.log("Conllect thành công"));
